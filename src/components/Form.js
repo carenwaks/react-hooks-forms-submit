6f98027ad3581d18bdeeb435be1c17da1f48462d
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-04-21 19:35:21 +0300

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -45,7 +45,7 @@ function Form() {
         <input type="text" onChange={handleLastNameChange} value={lastName} />
         <button type="submit">Submit</button>
       </form>
-      {/* conditionally render error messages */}
+      
       {errors.length > 0
         ? errors.map((error, index) => (
             <p key={index} style={{ color: "red" }}>
